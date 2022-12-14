ad676c537507e225d2765e62828284bed5a886e9
rabbi7sf1ght <113305087+rabbi7sf1ght@users.noreply.github.com>
2022-12-14 21:07:22 +0500

    Правда или действие (часть 1)

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,6 +4,7 @@ const bigPicture = document.querySelector('.big-picture');
 const closeBigPictureButton = document.querySelector('.big-picture__cancel');
 const commentTemplate = document.querySelector('.social__comment').cloneNode(true);
 const commentSection = document.querySelector('.social__comments');
+const bodyElement = document.querySelector('body');
 
 const alternateBigPictureData = (pictureData) => {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
@@ -13,28 +14,32 @@ const alternateBigPictureData = (pictureData) => {
   bigPicture.querySelector('.social__caption').textContent = pictureData.description;
 };
 
+const createComment = (commentData) => {
+  const comment = commentTemplate.cloneNode(true);
+  const commentPicture = comment.querySelector('img');
+  commentPicture.src = commentData.avatar;
+  commentPicture.alt = commentData.name;
+  comment.querySelector('p').textContent = commentData.message;
+  return comment;
+};
+
 const createCommentsView = (pictureData) => {
   commentSection.innerHTML = '';
   const commentFragment = document.createDocumentFragment();
   for (let i = 0; i < pictureData.comments.length; i++) {
-    const comment = commentTemplate.cloneNode(true);
-    const commentPicture = comment.querySelector('img');
-    commentPicture.src = pictureData.comments[i].avatar;
-    commentPicture.alt = pictureData.comments[i].name;
-    comment.querySelector('p').textContent = pictureData.comments[i].message;
-    commentFragment.appendChild(comment);
+    commentFragment.appendChild(createComment(pictureData.comments[i]));
   }
   return commentFragment;
 };
 
 const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
+  bodyElement.classList.remove('modal-open');
 };
 
 const openBigPicture = () => {
   bigPicture.classList.remove('hidden');
-  document.querySelector('body').classList.add('modal-open');
+  bodyElement.classList.add('modal-open');
 };
 
 const onCloseBigPictureButtonClick = () => {
