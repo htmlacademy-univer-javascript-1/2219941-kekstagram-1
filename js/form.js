5c8bdb64107f8d21f8e6fdde73980f9adc9aa9eb
rabbi7sf1ght <113305087+rabbi7sf1ght@users.noreply.github.com>
2022-12-22 01:47:50 +0500

    Работа с сервером

--- js/form.js
+++ js/form.js
@@ -2,12 +2,18 @@ import {isEscape} from './util.js';
 import {addHashtagValidator} from './hashtags.js';
 import {addZoomHandler, removeZoomHandler, resetZoom} from './zoom.js';
 import {resetEffects} from './effects.js';
+import {sendRequest} from './fetch.js';
 
 const bodyElement = document.querySelector('body');
 const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
-const uploadCloseButton = imgUploadForm.querySelector('#upload-cancel');
 const imgUploadControl = imgUploadForm.querySelector('#upload-file');
+const uploadCloseButton = imgUploadForm.querySelector('#upload-cancel');
+
+const successMessage =  document.querySelector('#success').content.querySelector('.success').cloneNode(true);
+const errorMessage = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
+const successCloseButton = successMessage.querySelector('.success__button');
+const errorCloseButton = errorMessage.querySelector('.error__button');
 
 const closeUploadForm = () => {
   imgUploadOverlay.classList.add('hidden');
@@ -43,8 +49,94 @@ const onUploadChangeHandler = () => {
   addHashtagValidator();
 };
 
-const addUploadFormHandler = () => {
-  imgUploadControl.addEventListener('change', onUploadChangeHandler);
+const closeSuccessMessage = () => {
+  successMessage.classList.add('hidden');
 };
 
-export {addUploadFormHandler};
+const closeErrorMessage = () => {
+  errorMessage.classList.add('hidden');
+  imgUploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onDocumentEscapeKeyDown);
+};
+
+const onSuccessDocumentClick = (evt) => {
+  if (evt.target.classList.contains('popup')) {
+    closeSuccessMessage();
+    document.removeEventListener('click', onSuccessDocumentClick);
+  }
+};
+
+const onErrorDocumentClick = (evt) => {
+  if (evt.target.classList.contains('popup')) {
+    closeErrorMessage();
+    document.removeEventListener('click', onErrorDocumentClick);
+  }
+};
+
+const onSuccessEscapeKeyDown = () => {
+  if (!successMessage.classList.contains('hidden')) {
+    closeSuccessMessage();
+    document.removeEventListener('click', onSuccessDocumentClick);
+    document.removeEventListener('keydown', onSuccessEscapeKeyDown);
+  }
+};
+
+const onErrorEscapeKeyDown = () => {
+  if (!errorMessage.classList.contains('hidden')) {
+    closeErrorMessage();
+    document.removeEventListener('click', onErrorDocumentClick);
+    document.removeEventListener('keydown', onErrorEscapeKeyDown);
+  }
+};
+
+const onSuccessButtonClick = () => {
+  closeSuccessMessage();
+  document.removeEventListener('click', onSuccessDocumentClick);
+  document.removeEventListener('keydown', onSuccessEscapeKeyDown);
+};
+
+const onErrorButtonClick = () => {
+  closeErrorMessage();
+  document.removeEventListener('click', onErrorDocumentClick);
+  document.removeEventListener('keydown', onErrorEscapeKeyDown);
+};
+
+const appendMessage = (message) => {
+  message.classList.add('hidden');
+  bodyElement.appendChild(message);
+};
+
+const onSuccess = () => {
+  closeUploadForm();
+  document.removeEventListener('keydown', onDocumentEscapeKeyDown);
+
+  successMessage.classList.remove('hidden');
+  document.addEventListener('click', onSuccessDocumentClick);
+  document.addEventListener('keydown', onSuccessEscapeKeyDown, {once: true});
+};
+
+const onError = () => {
+  imgUploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentEscapeKeyDown,);
+
+  errorMessage.classList.remove('hidden');
+  document.addEventListener('click', onErrorDocumentClick);
+  document.addEventListener('keydown', onErrorEscapeKeyDown, {once: true});
+};
+
+const onUploadFormSubmit = (evt) => {
+  evt.preventDefault();
+  sendRequest(onSuccess, onError, 'POST', new FormData(imgUploadForm));
+};
+
+const addFormEventsHandler = () => {
+  imgUploadControl.addEventListener('change', onUploadChangeHandler);
+  imgUploadForm.addEventListener('submit', onUploadFormSubmit);
+
+  appendMessage(successMessage);
+  appendMessage(errorMessage);
+  successCloseButton.addEventListener('click', onSuccessButtonClick);
+  errorCloseButton.addEventListener('click', onErrorButtonClick);
+};
+
+export {addFormEventsHandler};
--- js/util.js
+++ js/util.js
@@ -1,19 +1,14 @@
-const getRandomIntNumber = (min,max) => {
-  min = min < 0 ? Math.abs(min) : min;
-  max = max < 0 ? Math.abs(max) : max;
-  return min < max ? Math.floor(Math.random() * (max - min + 1) + min)
-    : Math.floor(Math.random() * (max - min + 1) + min);
-};
+const DIVIDER_TO_GET_UNITS = 10;
 
 const checkLineLength = (line, maxLength) => line.length <= maxLength;
 
 const isEscape = (evt) => evt.key === 'Escape';
 
 const changeWord = (firstForm, secondForm, marker) => {
-  if (marker % 10 === 1) {
+  if (marker % DIVIDER_TO_GET_UNITS === 1) {
     return firstForm;
   }
   return secondForm;
 };
 
-export {getRandomIntNumber, checkLineLength, isEscape, changeWord};
+export {checkLineLength, isEscape, changeWord};
